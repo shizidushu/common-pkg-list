cca64ba1a279e832ce6cb08d9c590dbe36f14e68
shizidushu <shizidushu@gmail.com>
2018-09-30 17:14:12 +0800

    Update r-pkgs-dev.R

--- r-pkgs-dev.R
+++ r-pkgs-dev.R
@@ -1,5 +1,32 @@
 if (!require(devtools)) install.packages("devtools")
 
+list_of_packages <- c("blogdown", "car", "config", "cronR", "devtools", "future", 
+                      "ggmap", "ggrepel", "ggthemes", "ISLR", "knitr", "magick", "mailR", 
+                      "odbc", "openxlsx", "plotly", "pool", "promises", "pryr", "quantmod", 
+                      "Rcpp", "redux", "rJava", "RJDBC", "rmarkdown", "RPostgreSQL", 
+                      "sjmisc", "styler", "tidypredict", "tidyverse", "viridis", "writexl", 
+                      "XML")
+
+# dput(list_of_packages[order(list_of_packages)])
+
+list_of_packages <- unique(list_of_packages)
+
+new_packages <- list_of_packages[! list_of_packages %in% installed.packages()[,"Package"] ]
+
+if(length(new_packages)) install.packages(new_packages)
+
+blogdown::install_hugo()
+devtools::install_github('shizidushu/hfun')
+
+devtools::install_github('rstudio/blogdown')
+devtools::install_github('ropensci/magick')
+update.packages(ask = FALSE, repos = 'https://cran.r-project.org')
+install.packages('knitr', repos = c('https://xran.yihui.name', 'https://cran.r-project.org'))
+devtools::install_github('trestletech/plumber')
+devtools::install_github("ropensci/writexl")
+devtools::install_github("r-lib/lobstr")
+
+
 devtools::install_github("r-lib/rlang", build_vignettes = TRUE)
 
 devtools::install_github("hadley/tidyverse")
@@ -19,29 +46,3 @@ devtools::install_github("tidyverse/hms")
 devtools::install_github("tidyverse/stringr")
 devtools::install_github("tidyverse/glue")
 devtools::install_github("tidyverse/forcats")
-
-devtools::install_github('rstudio/blogdown')
-devtools::install_github('ropensci/magick')
-update.packages(ask = FALSE, repos = 'https://cran.r-project.org')
-install.packages('knitr', repos = c('https://xran.yihui.name', 'https://cran.r-project.org'))
-devtools::install_github('trestletech/plumber')
-devtools::install_github("ropensci/writexl")
-devtools::install_github("r-lib/lobstr")
-
-list_of_packages <- c("blogdown", "car", "config", "cronR", "devtools", "future", 
-                      "ggmap", "ggrepel", "ggthemes", "ISLR", "knitr", "magick", "mailR", 
-                      "odbc", "openxlsx", "plotly", "pool", "promises", "pryr", "quantmod", 
-                      "Rcpp", "redux", "rJava", "RJDBC", "rmarkdown", "RPostgreSQL", 
-                      "sjmisc", "styler", "tidypredict", "tidyverse", "viridis", "writexl", 
-                      "XML")
-
-# dput(list_of_packages[order(list_of_packages)])
-
-list_of_packages <- unique(list_of_packages)
-
-new_packages <- list_of_packages[! list_of_packages %in% installed.packages()[,"Package"] ]
-
-if(length(new_packages)) install.packages(new_packages)
-
-blogdown::install_hugo()
-devtools::install_github('shizidushu/hfun')
