4872d7f1cda54993ab89265895f7566fe3fcb2e5
shizidushu <shizidushu@gmail.com>
2018-09-30 06:55:00 +0000

    new think

---
+++ r-pkg-dev.R
@@ -0,0 +1,24 @@
+install.packages("devtools")
+devtools::install_github("hadley/tidyverse")
+devtools::install_github('rstudio/blogdown')
+devtools::install_github('ropensci/magick')
+update.packages(ask = FALSE, repos = 'https://cran.r-project.org')
+install.packages('knitr', repos = c('https://xran.yihui.name', 'https://cran.r-project.org'))
+devtools::install_github('trestletech/plumber')
+devtools::install_github("r-lib/lobstr")
+
+blogdown::install_hugo()
+devtools::install_github('shizidushu/hfun', dependencies=TRUE)
+
+list_of_packages <- c("car", "config", "cronR", "future", "ggmap", "ggrepel", "ggthemes", 
+                      "tidypredict", "writexl", "XML", "ISLR", "sjmisc", "styler", 
+                      "viridis", "odbc", "plotly", "pool", "promises", "pryr", "quantmod", 
+                      "Rcpp", "rJava", "RPostgreSQL", "rmarkdown")
+
+# dput(list_of_packages[order(list_of_packages)])
+
+list_of_packages <- unique(list_of_packages)
+
+new_packages <- list_of_packages[! list_of_packages %in% installed.packages()[,"Package"] ]
+
+if(length(new_packages)) install.packages(new_packages)
---
+++ r-pkg.R
@@ -0,0 +1,19 @@
+list_of_packages <- c("blogdown", "car", "config", "cronR", "devtools", "future", 
+                      "ggmap", "ggrepel", "ggthemes", "ISLR", "knitr", "magick", "odbc", 
+                      "plotly", "pool", "promises", "pryr", "quantmod", "Rcpp", "rJava", 
+                      "rmarkdown", "RPostgreSQL", "sjmisc", "styler", "tidypredict", 
+                      "tidyverse", "viridis", "writexl", "XML")
+
+# dput(list_of_packages[order(list_of_packages)])
+
+list_of_packages <- unique(list_of_packages)
+
+new_packages <- list_of_packages[! list_of_packages %in% installed.packages()[,"Package"] ]
+
+if(length(new_packages)) install.packages(new_packages)
+
+devtools::install_github('trestletech/plumber')
+devtools::install_github("r-lib/lobstr")
+
+blogdown::install_hugo()
+devtools::install_github('shizidushu/hfun', dependencies=TRUE)
