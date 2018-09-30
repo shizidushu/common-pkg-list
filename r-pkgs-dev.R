63fb4784f25e66668613285e671d32531dda7efe
shizidushu <shizidushu@gmail.com>
2018-09-30 08:28:20 +0000

    install hfun last

--- r-pkgs-dev.R
+++ r-pkgs-dev.R
@@ -8,13 +8,12 @@ install.packages('knitr', repos = c('https://xran.yihui.name', 'https://cran.r-p
 devtools::install_github('trestletech/plumber')
 devtools::install_github("r-lib/lobstr")
 
-blogdown::install_hugo()
-devtools::install_github('shizidushu/hfun', dependencies=TRUE)
-
-list_of_packages <- c("car", "config", "cronR", "future", "ggmap", "ggrepel", "ggthemes", 
-                      "tidypredict", "writexl", "XML", "ISLR", "sjmisc", "styler", 
-                      "viridis", "odbc", "plotly", "pool", "promises", "pryr", "quantmod", 
-                      "Rcpp", "rJava", "RPostgreSQL", "rmarkdown")
+list_of_packages <- c("blogdown", "car", "config", "cronR", "devtools", "future", 
+                      "ggmap", "ggrepel", "ggthemes", "ISLR", "knitr", "magick", "mailR", 
+                      "odbc", "openxlsx", "plotly", "pool", "promises", "pryr", "quantmod", 
+                      "Rcpp", "redux", "rJava", "RJDBC", "rmarkdown", "RPostgreSQL", 
+                      "sjmisc", "styler", "tidypredict", "tidyverse", "viridis", "writexl", 
+                      "XML")
 
 # dput(list_of_packages[order(list_of_packages)])
 
@@ -23,3 +22,6 @@ list_of_packages <- unique(list_of_packages)
 new_packages <- list_of_packages[! list_of_packages %in% installed.packages()[,"Package"] ]
 
 if(length(new_packages)) install.packages(new_packages)
+
+blogdown::install_hugo()
+devtools::install_github('shizidushu/hfun')
