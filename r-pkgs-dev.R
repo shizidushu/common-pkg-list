9b35b2633406fbf1243833fd93ac89c254767f18
shizidushu <shizidushu@gmail.com>
2018-10-26 14:01:32 +0000

    separate dev

--- r-pkgs-dev.R
+++ r-pkgs-dev.R
@@ -1,26 +1,5 @@
 if (!require(devtools)) install.packages("devtools")
 
-list_of_packages <- c("blogdown", "car", "colorspace", "config", "cronR", "dbplyr", 
-                      "devtools", "DiagrammeR", "dygraphs", "faraway", "flexdashboard", 
-                      "future", "ggmap", "ggplot2", "ggrepel", "ggthemes", "glue", 
-                      "htmltools", "ISLR", "jsonlite", "knitr", "lubridate", "magick", 
-                      "magrittr", "mailR", "odbc", "openxlsx", "pander", "pixiedust", 
-                      "plotly", "pool", "promises", "pryr", "purrr", "quantmod", "rattle", 
-                      "Rcpp", "readr", "readxl", "redux", "remotes", "rJava", "RJDBC", 
-                      "rlang", "rmarkdown", "ROCR", "rpart.plot", "RPostgreSQL", "shinyjs", 
-                      "shinythemes", "showtext", "sjmisc", "storr", "styler", "swagger", 
-                      "tidypredict", "tidyverse", "tinytex", "urltools", "viridis", 
-                      "writexl", "XML")
-
-# dput(sort(list_of_packages))
-
-list_of_packages <- unique(list_of_packages)
-
-new_packages <- list_of_packages[! list_of_packages %in% installed.packages()[,"Package"] ]
-
-if(length(new_packages)) install.packages(new_packages)
-
-
 # It seems hugo installed for root user and other users need to install their own hugo
 # while (!file.exists("/root/bin/hugo")) {
 #   blogdown::install_hugo()
--- r-pkgs.R
+++ r-pkgs.R
@@ -1,20 +1,19 @@
-list_of_packages <- c("blogdown", "car", "config", "cronR", "devtools", "DiagrammeR", "future", 
-                      "ggmap", "ggrepel", "ggthemes", "ISLR", "knitr", "magick", "mailR", 
-                      "odbc", "openxlsx", "plotly", "pool", "promises", "pryr", "quantmod", 
-                      "Rcpp", "redux", "rJava", "RJDBC", "rmarkdown", "RPostgreSQL", 
-                      "sjmisc", "styler", "tidypredict", "tidyverse", "viridis", "writexl", 
-                      "XML")
+list_of_packages <- c("blogdown", "car", "colorspace", "config", "cronR", "dbplyr", 
+                      "devtools", "DiagrammeR", "dygraphs", "faraway", "flexdashboard", 
+                      "future", "ggmap", "ggplot2", "ggrepel", "ggthemes", "glue", 
+                      "htmltools", "ISLR", "jsonlite", "knitr", "lubridate", "magick", 
+                      "magrittr", "mailR", "odbc", "openxlsx", "pander", "pixiedust", 
+                      "plotly", "pool", "promises", "pryr", "purrr", "quantmod", "rattle", 
+                      "Rcpp", "readr", "readxl", "redux", "remotes", "rJava", "RJDBC", 
+                      "rlang", "rmarkdown", "ROCR", "rpart.plot", "RPostgreSQL", "shinyjs", 
+                      "shinythemes", "showtext", "sjmisc", "storr", "styler", "swagger", 
+                      "tidypredict", "tidyverse", "tinytex", "urltools", "viridis", 
+                      "writexl", "XML")
 
-# dput(list_of_packages[order(list_of_packages)])
+# dput(sort(list_of_packages))
 
 list_of_packages <- unique(list_of_packages)
 
 new_packages <- list_of_packages[! list_of_packages %in% installed.packages()[,"Package"] ]
 
 if(length(new_packages)) install.packages(new_packages)
-
-devtools::install_github('trestletech/plumber')
-devtools::install_github("r-lib/lobstr")
-
-blogdown::install_hugo()
-devtools::install_github('shizidushu/hfun')
