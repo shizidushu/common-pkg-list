7d190c138f81b81fb9dd51c5a1235ba911118170
shizidushu <shizidushu@gmail.com>
2018-11-26 16:09:47 +0000

    r-pkgs contains all needs. dev add devs

--- r-pkgs-dev.R
+++ r-pkgs-dev.R
@@ -5,7 +5,9 @@ list_of_packages <- c("blogdown", "brms", "clValid", "colorspace", "cronR", "Dia
                       "dygraphs", "ezknitr", "faraway", "ISLR", "leaflet", "magick", 
                       "pixiedust", "pryr", "quantmod", "rattle", "reticulate", "rio", 
                       "rJava", "RJDBC", "ROCR", "rpart.plot", "RPostgreSQL", "sf", 
-                      "storr", "tinytex", "tidypredict", "cowplot", "ggpubr")
+                      "storr", "tinytex", "tidypredict", "cowplot", "ggpubr", "reprex",
+                      "highcharter", "metricsgraphics", "crosstalk", "leaflet", "blogdown",
+                      "viridis","dbplyr","magick",  "hms")
 
 # dput(sort(unique(list_of_packages)))
 
@@ -22,53 +24,12 @@ if (!require(devtools)) install.packages("devtools")
 #   blogdown::install_hugo()
 # }
 
-
-devtools::install_github('shizidushu/hfun')
-
-
-devtools::install_github("rstudio/httpuv")
-devtools::install_github("ramnathv/htmlwidgets")
-devtools::install_github('rstudio/DT')
-devtools::install_github("jbkunst/highcharter")
-devtools::install_github("hrbrmstr/metricsgraphics")
-devtools::install_github("rstudio/crosstalk")
-devtools::install_github("jcheng5/d3scatter")
-devtools::install_github("rstudio/leaflet")
-#devtools::install_github("cosname/recharts")
-#devtools::install_github("JohnCoene/echarts4r")
-devtools::install_github("lchiffon/wordcloud2")
 devtools::install_github("rstudio/radix")
 
-devtools::install_github('rstudio/blogdown')
-remotes::install_github("richfitz/redux", upgrade = FALSE)
-devtools::install_github('ropensci/magick')
-update.packages(ask = FALSE, repos = 'https://cran.r-project.org')
-install.packages('knitr', repos = c('https://xran.yihui.name', 'https://cran.r-project.org'))
-devtools::install_github('trestletech/plumber')
-devtools::install_github("ropensci/writexl")
+devtools::install_github('shizidushu/hfun')
 devtools::install_github("r-lib/lobstr")
-
-
-
-
 devtools::install_github("r-lib/rlang", build_vignettes = TRUE)
 
-devtools::install_github("hadley/tidyverse")
-devtools::install_github("tidyverse/dplyr")
-devtools::install_github("tidyverse/ggplot2")
-devtools::install_github("tidyverse/readr")
-devtools::install_github("tidyverse/purrr")
-devtools::install_github("tidyverse/magrittr")
-devtools::install_github("tidyverse/tidyr")
-devtools::install_github("tidyverse/reprex")
-devtools::install_github("tidyverse/dbplyr")
-devtools::install_github("tidyverse/lubridate")
-devtools::install_github("tidyverse/tidyselect")
-devtools::install_github("tidyverse/modelr")
-devtools::install_github("tidyverse/readxl")
-devtools::install_github("tidyverse/hms")
-devtools::install_github("tidyverse/stringr")
-devtools::install_github("tidyverse/glue")
-devtools::install_github("tidyverse/forcats")
+#devtools::install_github("cosname/recharts")
+#devtools::install_github("JohnCoene/echarts4r")
 
-devtools::install_github("thomasp85/patchwork")
--- r-pkgs.R
+++ r-pkgs.R
@@ -1,16 +1,15 @@
 # install package that meant to be used
 if (!require(devtools)) install.packages("devtools")
 
-list_of_packages <- c("car", "config", "cowplot", "dbplyr", "devtools", "dplyr", 
-                      "DT", "flexdashboard", "formattable", "future", "ggmap", "ggplot2", 
-                      "ggrepel", "ggthemes", "glue", "htmltools", "htmlwidgets", "httpuv", 
-                      "httr", "jsonlite", "knitr", "lubridate", "magrittr", "mailR", 
-                      "odbc", "openxlsx", "optparse", "pander", "plotly", "plumber", 
-                      "pool", "promises", "purrr", "Rcpp", "readr", "readxl", "redux", 
-                      "remotes", "rlang", "rmarkdown", "shiny.router", "shinyjs", "shinythemes", 
+list_of_packages <- c("car", "config", "cowplot", "devtools", "dplyr", "DT", "flexdashboard", 
+                      "forcats", "formattable", "future", "ggmap", "ggplot2", "ggrepel", 
+                      "ggthemes", "glue", "htmltools", "htmlwidgets", "httpuv", "httr", 
+                      "jsonlite", "knitr", "lubridate", "magrittr", "mailR", "odbc", 
+                      "openxlsx", "optparse", "pander", "plotly", "plumber", "pool", 
+                      "promises", "purrr", "Rcpp", "readr", "readxl", "redux", "remotes", 
+                      "rlang", "rmarkdown", "shiny.router", "shinyjs", "shinythemes", 
                       "showtext", "sjmisc", "stringr", "styler", "swagger", "tidyr", 
-                      "tidyverse", "urltools", "viridis", "wordcloud2", "writexl", 
-                      "XML")
+                      "tidyverse", "urltools", "wordcloud2", "writexl", "XML")
 
 # dput(sort(unique(list_of_packages)))
 
@@ -19,3 +18,9 @@ list_of_packages <- unique(list_of_packages)
 new_packages <- list_of_packages[! list_of_packages %in% installed.packages()[,"Package"] ]
 
 if(length(new_packages)) install.packages(new_packages)
+
+devtools::install_github("thomasp85/patchwork")
+
+if (!dir.exists('/home/rstudio/bin/')) {dir.create('/home/rstudio/bin/')}
+decapitated::download_chromium('/home/rstudio/bin/')
+write('HEADLESS_CHROME=/home/rstudio/bin/chrome-linux/chrome',file="/usr/local/lib/R/etc/Renviron",append=TRUE)
