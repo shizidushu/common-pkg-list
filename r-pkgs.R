b9ff69cce2529145bc76f0888da0b82ccd99b67a
shizidushu <shizidushu@gmail.com>
2018-11-27 00:41:43 +0800

    remove trailing /

--- r-pkgs.R
+++ r-pkgs.R
@@ -23,5 +23,5 @@ devtools::install_github("thomasp85/patchwork")
 
 devtools::install_github("hrbrmstr/decapitated")
 if (!dir.exists('/home/rstudio/bin/')) {dir.create('/home/rstudio/bin/')}
-decapitated::download_chromium('/home/rstudio/bin/')
+decapitated::download_chromium('/home/rstudio/bin')
 write('HEADLESS_CHROME=/home/rstudio/bin/chrome-linux/chrome',file="/usr/local/lib/R/etc/Renviron",append=TRUE)
