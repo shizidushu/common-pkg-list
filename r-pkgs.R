2f87e04bced7951464067c8d33a6b35818f211c9
shizidushu <shizidushu@gmail.com>
2019-02-13 16:50:15 +0800

    move some package to dev

--- r-pkgs.R
+++ r-pkgs.R
@@ -1,15 +1,15 @@
 # install package that meant to be used
 if (!require(devtools)) install.packages("devtools")
 
-list_of_packages <- c("car", "config", "cowplot", "devtools", "dplyr", "DT", 
-                      "flexdashboard", "forcats", "formattable", "future", "gganimate", 
+list_of_packages <- c("car", "config",  "devtools", "dplyr", "DT", 
+                      "forcats",  "future", 
                       "ggplot2", "ggrepel", "ggpubr", "ggthemes", "glue", "htmltools", 
                       "htmlwidgets", "httpuv", "httr", "jose", "jsonlite", "knitr", 
-                      "lubridate", "magrittr", "mailR", "mindr", "odbc", "openxlsx", 
-                      "optparse", "pander", "plotly", "plumber", "pool", "promises", 
+                      "lubridate", "magrittr", "mailR", "odbc", "openxlsx", 
+                      "optparse",  "plotly", "plumber", "pool", "promises", 
                       "purrr", "Rcpp", "readr", "readxl", "rebus", "redux", 
-                      "remotes", "rlang", "rmarkdown", "shinydashboard", "shinyjs", 
-                      "shinythemes", "showtext", "sjmisc", "stringr", "styler", "swagger", 
+                      "rlang", "rmarkdown", "shinydashboard", "shinyjs", 
+                      "shinythemes", "showtext", "sjmisc", "stringr",
                       "tidyr", "tidyverse", "urltools", "wordcloud2", "writexl", "XML"
 )
 
@@ -19,7 +19,7 @@ list_of_packages <- unique(list_of_packages)
 
 new_packages <- list_of_packages[! list_of_packages %in% installed.packages()[,"Package"] ]
 
-if(length(new_packages)) install.packages(new_packages, quiet = TRUE)
+if(length(new_packages)) install.packages(new_packages)
 
 devtools::install_github('shizidushu/hfun')
 
