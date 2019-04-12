965f02776acd959e08e79f6a837f1ffeaa164702
shizidushu <shizidushu@gmail.com>
2019-04-12 05:11:16 +0000

    separate shiny pkg from complete-scheme

---
+++ r-pkgs-shiny.R
@@ -0,0 +1,20 @@
+# install package that meant to be used
+if (!require(devtools)) install.packages("devtools")
+
+# remotes::install_github("r-lib/remotes")
+
+list_of_packages <- c("plotly", "shinydashboard", "shinyjs", "shinythemes", "shinyWidgets")
+
+# dput(sort(unique(list_of_packages)))
+
+list_of_packages <- unique(list_of_packages)
+
+new_packages <- list_of_packages[! list_of_packages %in% installed.packages()[,"Package"] ]
+
+if(length(new_packages)) install.packages(new_packages, quiet = TRUE)
+
+install.packages(attr(old.packages(), "dimnames")[[1]])
+
+
+devtools::install_github('bbc/bbplot', quiet = TRUE)
+remotes::install_github("rstudio/gt", quiet = TRUE)
--- r-pkgs.R
+++ r-pkgs.R
@@ -1,17 +1,15 @@
 # install package that meant to be used
 if (!require(devtools)) install.packages("devtools")
 
-# remotes::install_github("r-lib/remotes")
 
 list_of_packages <- c("blastula", "car", "config", "devtools", "dplyr", "DT", "forcats", 
                       "future", "ggforce", "ggplot2", "ggpubr", "ggrepel", "ggthemes", 
                       "glue", "htmltools", "htmlwidgets", "httpuv", "httr", "jose", 
                       "jsonlite", "knitr", "lubridate", "magrittr", "mailR", "odbc", 
-                      "openxlsx", "optparse", "plotly", "plumber", "pool", "promises", 
-                      "purrr", "Rcpp", "readr", "readxl", "rebus", "redux", "reticulate", 
-                      "rlang", "rlist", "rmarkdown", "shinydashboard", "shinyjs", "shinythemes", 
-                      "shinyWidgets", "showtext", "sjmisc", "stringr", "tidyr", "tidyverse", 
-                      "urltools", "wordcloud2", "writexl", "XML")
+                      "openxlsx", "optparse", "plumber", "pool", "promises", "purrr", 
+                      "Rcpp", "readr", "readxl", "rebus", "redux", "reticulate", "rlang", 
+                      "rlist", "rmarkdown", "showtext", "sjmisc", "stringr", "tidyr", 
+                      "tidyverse", "urltools", "wordcloud2", "writexl", "XML")
 
 # dput(sort(unique(list_of_packages)))
 
@@ -24,7 +22,5 @@ if(length(new_packages)) install.packages(new_packages, quiet = TRUE)
 install.packages(attr(old.packages(), "dimnames")[[1]])
 
 devtools::install_github('shizidushu/hfun', quiet = TRUE)
-devtools::install_github('bbc/bbplot', quiet = TRUE)
-remotes::install_github("rstudio/gt", quiet = TRUE)
 
 
