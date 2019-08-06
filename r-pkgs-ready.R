1a8e8c6eee8f609bc3ef783a6bc36b9d6bcb4156
shizidushu <shizidushu@gmail.com>
2019-08-06 02:11:27 +0000

    add rstan first

--- r-pkgs-ready.R
+++ r-pkgs-ready.R
@@ -1,6 +1,8 @@
 # install package that I at least know a bit
 # not ready for production
 
+message("install packages from r-pkgs-ready.R")
+
 if (!require(remotes)) {
   install.packages('remotes', repo = 'https://cran.rstudio.com/')
 } else if (packageVersion("remotes") < "2.0.4") {
@@ -8,6 +10,20 @@ if (!require(remotes)) {
 }
 
 if (!require(devtools)) install.packages("devtools", repo = 'https://cran.rstudio.com/')
+
+
+dotR <- file.path(Sys.getenv("HOME"), ".R")
+if (!file.exists(dotR)) dir.create(dotR)
+M <- file.path(dotR, "Makevars")
+if (!file.exists(M)) file.create(M)
+cat("\nCXX14FLAGS=-O3 -march=native -mtune=native -fPIC",
+    "CXX14=g++", # or clang++ but you may need a version postfix
+    file = M, sep = "\n", append = TRUE)
+
+Sys.setenv(MAKEFLAGS = paste0("-j", parallel::detectCores()))
+
+install.packages("rstan", type = "source")
+
 
 # -rstudio
 list_of_packages <- c("flexdashboard", "formattable", "gganimate", 
