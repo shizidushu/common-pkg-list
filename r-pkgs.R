78574d3d6e41cb3822b6f19dae5cf2c73f2f5ac4
shizidushu <shizidushu@gmail.com>
2020-03-28 05:15:23 +0000

    add data.table patchwork ggthemes ggrepel

--- r-pkgs.R
+++ r-pkgs.R
@@ -13,11 +13,12 @@ if (!require(devtools)) install.packages("devtools", repo = 'https://cran.rstudi
 # - ariflow
 # - plumber
 # - shiny
-list_of_packages <- c("config", "DT", "future", "glue", "htmltools", 
-                      "htmlwidgets", "httpuv", "jose", "JuliaCall", "knitr", "loggit", 
-                      "mailR", "odbc", "openxlsx", "optparse", "pool", "readxl", 
-                      "rebus", "redux", "reticulate", "rlist", "rmarkdown", "sjmisc", 
-                      "snakecase", "tidyverse", "wordcloud2", "writexl", "XML", "plotly", "RPostgreSQL")
+list_of_packages <- c("config", "data.table", "DT", "future", "ggrepel", "ggthemes", 
+                      "glue", "htmltools", "htmlwidgets", "httpuv", "jose", "JuliaCall", 
+                      "knitr", "loggit", "mailR", "odbc", "openxlsx", "optparse", "patchwork", 
+                      "plotly", "pool", "readxl", "rebus", "redux", "reticulate", "rlist", 
+                      "rmarkdown", "RPostgreSQL", "sjmisc", "snakecase", "tidyverse", 
+                      "wordcloud2", "writexl", "XML")
 
 # dput(sort(unique(list_of_packages)))
 
