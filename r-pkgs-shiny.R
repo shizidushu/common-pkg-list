a34cfd70171fa509da910e192e890dc18dbeaf2f
shizidushu <shizidushu@gmail.com>
2020-03-29 07:30:32 +0000

    add listviewer

--- r-pkgs-shiny.R
+++ r-pkgs-shiny.R
@@ -10,7 +10,7 @@ if (!require(remotes)) {
 if (!require(devtools)) install.packages("devtools", repo = 'https://cran.rstudio.com/')
 
 # -shiny
-list_of_packages <- c("promises", "shinydashboard", "shinyWidgets",  "shinyjs", "shinyTime")
+list_of_packages <- c("promises", "shinydashboard", "shinyWidgets",  "shinyjs", "shinyTime", "listviewer")
 
 # dput(sort(unique(list_of_packages)))
 
